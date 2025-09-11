ffc26c26014413aa5dd2c4a54d78c6770e2f0fa4
batmanleo <92765344+batmanleo@users.noreply.github.com>
2025-09-10 18:06:08 -0700

    Add files via upload

---
+++ namenameington.java
@@ -0,0 +1,18 @@
+public class namenameington {
+public static void main(String[] args){
+
+String myname = "Leo";
+    int myage = 15;
+
+String broname = "Lucas";
+    int broage = 20;
+
+
+    System.out.print(myname);
+    System.out.println(myage);
+    System.out.print(broname);
+    System.out.print(broage);
+
+    } 
+}
+// sorry I didn't see the video you sent me until now

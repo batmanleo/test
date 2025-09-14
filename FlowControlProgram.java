81d622f192643964534c64a6ff36c50e9eda22ef
batmanleo <92765344+batmanleo@users.noreply.github.com>
2025-09-14 13:57:18 -0700

    Update FlowControlProgram.java

--- FlowControlProgram.java
+++ FlowControlProgram.java
@@ -2,17 +2,25 @@
 
 public class FlowControlProgram {
 public static void main(String[] args) {
+  int totalsum = 0;
 
   // This is the code for the loop and counting 1 to 100
 
     for (int number = 0; number <= 100; number++) {
-  System.out.println(number);
+    System.out.print(number);
 
-  // This is the code for telling if it's even or odd
+      // This is the code for telling if it's even or odd
 
-if (number%2 == 1) {
-   System.out.print("even ");} 
-   else { System.out.print("odd  "); }   
+      if (number%2 == 0) {
+      System.out.print(" even");} 
+      else { System.out.print(" odd "); }   
+
+      // This is the code for the sum of the numbers
+
+      System.out.print(" Sum ");
+      totalsum = totalsum+number; 
+      System.out.println(totalsum); 
+      
 
     }
   }

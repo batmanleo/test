d78112aa714953adf42ccc0e4fe66ffc9bc2fe46
batmanleo <92765344+batmanleo@users.noreply.github.com>
2025-10-07 09:24:03 -0700

    Update ojecttesting.java

--- ojecttesting.java
+++ ojecttesting.java
@@ -1,14 +1,16 @@
+// This is a program by Leo
+// This creates a few horses and out puts there name and birthday
+
 public class ojecttesting {
 public static void main(String[] args) {
     
-
-Arraylist<horse> objects=new Arraylist<horse>();  
-
+// This sets each horse
 
 horse horse1 = new horse("Patrick",2012);
 horse horse2 = new horse("Johnny",2011);
 horse horse3 = new horse("Gyro",2004);
 
+// This prints each horse
 
 System.out.println("First horse's name: " + horse1.name);
 System.out.println("First horse's birthday: " + horse1.birthYear);
@@ -36,4 +38,3 @@ public class horse {
 
     }
     }
-

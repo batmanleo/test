f666587c303e4e5099886e2a2d007549af2dd134
batmanleo <92765344+batmanleo@users.noreply.github.com>
2025-10-31 13:56:06 -0700

    Update JokerCard.java

--- JokerCard.java
+++ JokerCard.java
@@ -2,18 +2,18 @@ public class JokerCard extends Card{
 
     boolean IsWildCard;
 
-    public JokerCard(String suit, boolean IsWildCard){
+    public JokerCard(String colour, boolean IsWildCard){
         
-        super(0,suit);
+        super(0, "Jokers");
         this.IsWildCard = IsWildCard;
-       
+        this.colour = colour;
     }
 
     // this is for printing the card
     @Override
     public String toString() {
         if (IsWildCard){
-            return suit + " " + "Joker";
+            return colour + " " + "Joker";
         }
         return super.toString();
     }

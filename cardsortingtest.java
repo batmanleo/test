5d5e8d53c71afcf83759cce31580301fb7f4867d
batmanleo <92765344+batmanleo@users.noreply.github.com>
2025-11-10 09:39:15 -0800

    Add files via upload

---
+++ cardsortingtest.java
@@ -0,0 +1,17 @@
+public class cardsortingtest {
+public static void main(String[] args) {
+
+     // This makes 3 new cards and sets the number and suit
+    Card card1 = new Card(4, "spades");
+    Card card2 = new Card(4,"hearts");
+    Card card3 = new Card(1,"spades");
+  
+
+
+            // This draws the cards
+            card1.drawcard();
+            card2.drawcard();
+            card3.drawcard();
+
+}
+}

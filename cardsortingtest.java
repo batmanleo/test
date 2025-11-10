dad8ea23c0706b57c9b8915f2e7ae8e62e8b047f
batmanleo <92765344+batmanleo@users.noreply.github.com>
2025-11-10 09:40:44 -0800

    Update cardsortingtest.java

--- cardsortingtest.java
+++ cardsortingtest.java
@@ -1,17 +1,21 @@
+import java.util.*;
 public class cardsortingtest {
+     static Arraylist<Card> deck; 
 public static void main(String[] args) {
+
+deck = new Arraylist<Card>();
 
      // This makes 3 new cards and sets the number and suit
     Card card1 = new Card(4, "spades");
     Card card2 = new Card(4,"hearts");
     Card card3 = new Card(1,"spades");
-  
+    deck.add (card1);
+    deck.add (card2);
+    deck.add (card3);
+            
+            
+            compareTo;
 
-
-            // This draws the cards
-            card1.drawcard();
-            card2.drawcard();
-            card3.drawcard();
 
 }
 }

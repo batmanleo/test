9d5c0ea64689033ebf045f6eda41a77c3a46f6e0
batmanleo <92765344+batmanleo@users.noreply.github.com>
2025-11-11 17:45:54 -0800

    Add files via upload

---
+++ divide.java
@@ -0,0 +1,36 @@
+public class divide {
+public static void main(String[] args) {
+
+int mathingthing = 11; 
+int mathington = 0;
+try {
+System.out.println(mathingthing / mathington);
+} catch (Exception e) {
+    System.out.println(e);
+    System.out.println("you tried to divide by zero, an error happened");
+    System.out.print("you tried to ");
+    
+}
+
+
+System.out.println("divided something");
+
+int[]bob = new int[5];
+try {
+
+for(int x = 0; x<10; x++){
+
+    bob[x]=x;
+    System.out.println(x);
+}
+} catch (Exception e)
+
+{
+    System.out.println(e);
+    System.out.println("you tried to put to much in the array");
+}
+
+System.out.println("if this printed the code ran");
+
+}
+}

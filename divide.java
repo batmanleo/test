5b22da2fbc1ef2dac15e84be0a2b401b1b47e0a3
batmanleo <92765344+batmanleo@users.noreply.github.com>
2025-11-11 18:40:07 -0800

    Update divide.java
    Added comments to clarify exception handling and array limits.

--- divide.java
+++ divide.java
@@ -1,8 +1,13 @@
 public class divide {
 public static void main(String[] args) {
 
+
+
 int mathingthing = 11; 
 int mathington = 0;
+
+//this is the exception
+
 try {
 System.out.println(mathingthing / mathington);
 } catch (Exception e) {
@@ -15,8 +20,15 @@ System.out.println(mathingthing / mathington);
 
 System.out.println("divided something");
 
+//this array only can hold 6 things (0,1,2,3,4,5)
+
 int[]bob = new int[5];
+
+//this is the exception
+
 try {
+
+//this for loop tries to add 10 things to the array
 
 for(int x = 0; x<10; x++){
 
@@ -29,6 +41,8 @@ for(int x = 0; x<10; x++){
     System.out.println(e);
     System.out.println("you tried to put to much in the array");
 }
+
+//this is proof the code ran all the way through
 
 System.out.println("if this printed the code ran");
 
